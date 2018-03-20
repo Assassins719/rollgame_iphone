35bcb718cc45e8bbc27308a0580e4686b41b1fc2
TwinkleStar <twinklestar871025@hotmail.com>
2018-03-21 00:11:24 +0800

    Initial Commit

---
+++ 欢乐大转盘/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  欢乐大转盘
+//
+//  Created by TwinkleStar on 3/21/18.
+//  Copyright © 2018 yznrollret. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 欢乐大转盘/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  欢乐大转盘
+//
+//  Created by TwinkleStar on 3/21/18.
+//  Copyright © 2018 yznrollret. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
